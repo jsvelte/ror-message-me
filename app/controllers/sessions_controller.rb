3485b882abf8ba8a4fc385d57edf5b6483d85268
jsvelte <joshua.galit@sun-asterisk.com>
2023-04-09 21:11:21 +0800

    [MM-275] - [FE/BE] Display Chat Messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if (user && user.authenticate(params[:session][:password]))
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "You have successfully logged in"
+    else
+      flash[:alert] = "There was something wrong with your login information"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "You have successfully logged out"
+  end
 end

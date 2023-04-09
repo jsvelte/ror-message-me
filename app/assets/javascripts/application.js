ba0dd0f77bcf6ee4f82b2402e1453ce13a97b429
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2023-04-09 21:14:42 +0800

    [MM-275] - [FE/BE] Display Chat Messages (#8)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,7 @@ $(document).on("turbolinks:load", function () {
   $(".menu a").removeClass("active");
   $(".ui.dropdown").dropdown();
 });
+
+$(".message .close").on("click", function () {
+  $(this).closest(".message").transition("fade");
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  # Log In
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   root 'chatroom#index'
 end

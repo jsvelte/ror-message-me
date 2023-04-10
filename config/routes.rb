45e8c74c8ea6e32aea8ab13ddc551f99e87d6a9d
jsvelte <joshua.galit@sun-asterisk.com>
2023-04-10 16:25:00 +0800

    [MM-685] - [FE/BE] Implement Add Message Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
   # Log In
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  post 'message', to: 'messages#create'
 
   root 'chatroom#index'
 end
